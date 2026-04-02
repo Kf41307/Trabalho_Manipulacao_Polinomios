69e4eaf5ad7d52a9c16525e0384f451a66e21e04
Guilherme Krieger Ferreira <guilhermekf7@gmail.com>
2026-04-01 21:55:28 -0300

    Operações, Remoção e impressão Monômios

--- Polinomios.hpp
+++ Polinomios.hpp
@@ -43,7 +43,7 @@ bool inserirOrdem(Polinomio &lista, float coefNum, int expoente){ //Falta adicio
 
     if(lista.primeiro == NULL){ // Se a lista nao tiver nenhum elemento
         return inserirFinal(lista, coefNum, expoente);
-    }else if(expoente > lista.primeiro->expoente){ //Se o expoente do adicionado for maior que o primeiro
+    }else if((expoente > lista.primeiro->expoente)){ //Se o expoente do adicionado for maior que o primeiro
         No* novoMonomio = new No;
 
         novoMonomio->expoente = expoente;
@@ -67,7 +67,7 @@ bool inserirOrdem(Polinomio &lista, float coefNum, int expoente){ //Falta adicio
                 novoMonomio->proximo = atual;
                 return true;
             }else if(expoente == atual->expoente){
-                // funcao SOMAR(auxliar.coef, coefNum)
+                // funcao SOMAR(atual.coef, coefNum) coefNum do expoente
             }
 
             anterior = atual;
@@ -75,6 +75,174 @@ bool inserirOrdem(Polinomio &lista, float coefNum, int expoente){ //Falta adicio
         }
     }
     return false;
+}
+
+bool removerMonomio(Polinomio &lista, int expoente){
+    No* atual = lista.primeiro;
+    No* anterior = NULL;
+
+    if(lista.primeiro == NULL) return false;
+    
+    while(atual != NULL){
+        if(atual->expoente == expoente){
+
+            if((atual == lista.primeiro) && (atual == lista.ultimo)){
+                inicializar(lista);
+            }else if(atual == lista.primeiro){ //&& (atual != lista.ultimo)
+                lista.primeiro = atual->proximo;
+            }else if(atual == lista.ultimo){
+                lista.ultimo = anterior;
+                anterior->proximo = NULL;
+            }else{
+                anterior->proximo = atual->proximo;
+            }
+            delete atual;
+            return true;
+        }
+        anterior = atual;
+        atual = atual->proximo;
+    }
+    return false;
+}
+
+bool buscarExpoente(Polinomio lista, int expoente){
+    No* atual = lista.primeiro;
+
+    while(atual != NULL){
+        if(atual->expoente == expoente) return true;
+        atual = atual->proximo;
+    }
+
+    return false;
+}
+
+void imprimirPolinomio(Polinomio lista){ //Falta espaco no exp == 0 e coef < 0 antes do numero
+    No* atual = lista.primeiro;
+
+    while(atual != NULL){
+        if((atual == lista.primeiro) && (atual->coefNum > 0)){
+            cout<<atual->coefNum<<"x^"<<atual->expoente<<" ";
+        }else if((atual->expoente == 0) && atual->coefNum > 0){
+            cout<<"+ "<<atual->coefNum<<" ";
+        }else if(atual->expoente == 0){
+            cout<<atual->coefNum<<" ";
+        }else if(atual->expoente == 1){
+            cout<<atual->coefNum<<"x"<<" ";
+        }else if(atual->coefNum > 0){
+            cout<<"+ "<<atual->coefNum<<"x^"<<atual->expoente<<" ";
+        }else{
+            cout<<atual->coefNum<<"x^"<<atual->expoente<<" ";
+        }
+
+        atual = atual->proximo;
+    }
+}
+
+Polinomio somarPolinomios(Polinomio lista1, Polinomio lista2){
+
+    Polinomio soma;
+    inicializar(soma);
+
+    No* atual1 = lista1.primeiro;
+    No* atual2 = lista2.primeiro;
+
+    while(atual1 != NULL){
+
+        atual2 = lista2.primeiro;
+        bool jaSomou = false;
+
+        while(atual2 != NULL){
+            if(atual1->expoente == atual2->expoente){
+                float somaCoef = atual1->coefNum + atual2->coefNum;
+
+                if(somaCoef != 0){
+                    inserirOrdem(soma, somaCoef, atual1->expoente); //(5x^4 + 2x) +  2x^4 + x^2 =  7x^4 + 2x + x^2
+                }
+                jaSomou = true;
+            }
+            atual2 = atual2->proximo;
+        }
+
+        if(!jaSomou){
+            inserirOrdem(soma, atual1->coefNum, atual1->expoente);
+        }
+        atual1 = atual1->proximo;
+    }
+
+    atual2 = lista2.primeiro;
+    while(atual2 != NULL){
+        if(!buscarExpoente(lista1, atual2->expoente)){
+            inserirOrdem(soma, atual2->coefNum, atual2->expoente);
+        }
+        atual2 = atual2->proximo;
+    }
+
+    return soma;
+}
+
+void multiplicarPorEscalar(Polinomio &lista, float escalar){
+    No* atual = lista.primeiro;
+
+    if(lista.primeiro == NULL) return;
+
+    while(atual != NULL){
+        atual->coefNum *= escalar;
+
+        atual = atual->proximo;
+    }
+}
+
+Polinomio subtrairPolinomios(Polinomio lista1, Polinomio lista2){
+    multiplicarPorEscalar(lista2, -1);
+    Polinomio subtracao = somarPolinomios(lista1, lista2);
+    multiplicarPorEscalar(lista2, -1);
+
+    return subtracao;
+}
+
+Polinomio multiplicarPolinomios(Polinomio lista1, Polinomio lista2){
+ 
+    No* atual1 = lista1.primeiro;
+    No* atual2 = lista2.primeiro;
+
+    Polinomio multiplicacao;
+    inicializar(multiplicacao);
+
+    while(atual1 != NULL){
+          
+        atual2 = lista2.primeiro;
+
+        while(atual2 != NULL){
+            int somaExp = atual1->expoente + atual2->expoente;
+            float multCoef = atual1->coefNum * atual2->coefNum;
+
+            inserirOrdem(multiplicacao, multCoef, somaExp);
+        }
+        
+        atual1 = atual1->proximo;
+    }
+
+    //somarMonomiosMesmoGrau
+    return multiplicacao;
+}   
+
+float valorNumerico(Polinomio lista, float valorX){
+    No* atual = lista.primeiro;
+
+    float valorNumerico = 0;
+
+    while(atual != NULL){
+        float potencia = 1;
+        for(int i = 0; i < atual->expoente; i++){
+            potencia *= valorX;
+        }
+        
+        valorNumerico += atual->coefNum * potencia;
+
+        atual = atual->proximo;
+    }
+
+    return valorNumerico;
 }
 
 void liberar(Polinomio &lista){
@@ -87,4 +255,3 @@ void liberar(Polinomio &lista){
     }
     inicializar(lista);
 }
-
--- main.cpp
+++ main.cpp
@@ -4,6 +4,53 @@ int main(){
     system("chcp 65001");
     system("cls");
 
+    Polinomio x;
+    inicializar(x);
+    inserirOrdem(x, 2, 2);
+    inserirOrdem(x, -2, 0);
+    inserirOrdem(x, -2, 5);
+    // ERRO:  inserirOrdem(x, 1, 1);
     
+    Polinomio y;
+    inicializar(y);
+    inserirOrdem(y, 1, 2);
+    inserirOrdem(y, 7, 5);
+    inserirOrdem(y, 2, 0);
+    inserirOrdem(y, 1, 7);
+
+
+    Polinomio z;
+    inicializar(z);
+    z = somarPolinomios(x, y);
+    imprimirPolinomio(z);
+
+    cout<<endl<<endl;
+
+    Polinomio o;
+    inicializar(o);
+    o = subtrairPolinomios(x, y);
+    imprimirPolinomio(o);
+
+    cout<<endl<<"Valor numerico do polinomio x (com x = 1):"<<valorNumerico(y, 1);
+
+
+    Polinomio a;
+    inicializar(a);
+    inserirOrdem(a, 1, 3);
+    inserirFinal(a, 1, 1);
+    inserirOrdem(a, 2, 2);
+
+    imprimirPolinomio(a);
+    // Polinomio b;
+    // inicializar(b);
+    // inserirOrdem(b, 1, 1);
+    // inserirOrdem(b, 1, 0);
+
+    // Polinomio c;
+    // inicializar(c);
+    // c = multiplicarPolinomios(a, b);
+    // imprimirPolinomio(c);
+
+
     return 0;
 }
